42428dd1c2b6c24c7bef7565b4469ad8b312c449
davoodnazarifar2-source <davoodnazarifar2@gmail.com>
2026-05-01 20:24:13 +0330

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const TARGET_BASE = (process.env.TARGET_DOMAIN || "").replace(/\/$/, "");
 
 const STRIP_HEADERS = new Set([
